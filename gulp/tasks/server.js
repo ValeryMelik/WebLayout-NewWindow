e52dafc7df7d26b38bf87def73d5ce301f2790b9
Валерий Мелик-Шахназаров <100619837+ValeryMelik@users.noreply.github.com>
2024-09-02 00:48:16 +0300

    hot fixes

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,15 +1,13 @@
-const page = 'main'; // Название страницы, для которой настраивается сервер
-
 export function server(done) {
-  app.plugins.browserSync.create(page).init({
+  app.plugins.browserSync.create('index').init({
     server: {
       baseDir: app.path.build.root,
-      index: `${page}.html`,
+      index: `index.html`,
     },
     notify: false,
-    port: 3000, 
+    port: 3000,
     ui: {
-      port: 3001, 
+      port: 3001,
     },
   });
   done();
