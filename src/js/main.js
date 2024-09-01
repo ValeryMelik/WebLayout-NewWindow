21a183759eeed13ca11a06e550966fa479015cae
Валерий Мелик-Шахназаров <100619837+ValeryMelik@users.noreply.github.com>
2024-09-01 19:15:20 +0300

    prod-1

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import './modules/smooth-scroll.js';
-import './modules/map.js';
-import './modules/modal-1.js';
-import './modules/validate.js';
-import './modules/showmore.js';
-import './modules/accordion.js';
-import './modules/burger.js';
