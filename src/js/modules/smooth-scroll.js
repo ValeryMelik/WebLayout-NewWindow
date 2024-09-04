a8aa65c15e355ec1b12f043b24a806e950258805
Valera <you@example.com>
2024-09-04 10:56:28 +0300

    small fixes

--- src/js/modules/smooth-scroll.js
+++ src/js/modules/smooth-scroll.js
@@ -1,13 +1,16 @@
 ['products', 'portfolio', 'about-us', 'manufacture', 'contacts'].forEach(
   (item) => {
     const section = document.getElementById(`${item}-section`);
-
-    if (section)
-      document.getElementById(`${item}-link`).addEventListener('click', () => {
-        window.scrollTo({
-          top: section.getBoundingClientRect().top,
-          behavior: 'smooth',
+    if (section) {
+      document
+        .getElementById(`${item}-link`)
+        .addEventListener('click', (event) => {
+          event.preventDefault();
+          section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+          });
         });
-      });
+    }
   }
 );
